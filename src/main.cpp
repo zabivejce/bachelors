fef179447f61d2201b0954f9d7d68f56a2923551
ZabiVejce <jirka@zabaq.monster>
2025-10-12 13:51:42 +0200

    simple demod + audio

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,5 @@
+#include <algorithm>
+#include <ctime>
 #include <iostream>
 #include <cstdint>
 /*
@@ -5,13 +7,47 @@ https://k3xec.com/packrat-processing-iq/
 https://htorp.folk.ntnu.no/Undervisning/TTK10/IQdemodulation.pdf
 https://en.wikipedia.org/wiki/In-phase_and_quadrature_components
 */
+
+const int sdr_rate = 2400000;
+const int audio_rate = 48000;
+const int decimation = sdr_rate / audio_rate;
+const int gain = 16000;
+
+int count = 0;
+float tmp = 0.0f;
+bool lpf_dec(float x, float& y)
+{
+    tmp += x;
+    ++count;
+    if(count == decimation)
+    {
+        y = tmp / decimation;
+        tmp = 0.0f;
+        count = 0;
+        return true;
+    }
+    return false;
+}
 int main()
 {
     uint8_t iq[2];
+    float i,q,last_i,last_q;
+    float demod,audio;
     while(std::cin.read(reinterpret_cast<char*>(iq),2))
     {
-        float i = ((float)iq[0] - 127.5f) / 127.5f;
-        float q = ((float)iq[1] - 127.5f) / 127.5f;
+        i = ((float)iq[0] - 127.5f) / 127.5f;
+        q = ((float)iq[1] - 127.5f) / 127.5f;
+
+        demod = (last_i * q - last_q * i);
+        if(lpf_dec(demod, audio))
+        {
+            float scaled = audio * (float)gain;
+            int16_t sample = static_cast<int16_t>(std::clamp(scaled, -32767.0f, 32767.0f));
+            std::cout.write(reinterpret_cast<char*>(&sample), sizeof(sample));
+        }
+
+        last_i = i;
+        last_q = q;
     }
     return 0;
 }
