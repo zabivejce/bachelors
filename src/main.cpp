b4dcab892289ce787713b7ed1a2519809113fa8f
Jiri Hruby <hrubyjiri4@gmail.com>
2025-12-17 22:46:18 +0100

    filter 2 do

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,13 @@
 #include <algorithm>
 #include <asm-generic/socket.h>
 #include <cmath>
+#include <condition_variable>
 #include <iostream>
 #include <cstdint>
+#include <memory>
+#include <mutex>
+#include <queue>
+#include <thread>
 #include <vector>
 #include <stdexcept>
 #include <cstring>
@@ -36,6 +41,8 @@ const int sdr_rate = 2400000;
 const int audio_rate = 48000;
 const int decimation = sdr_rate / audio_rate;
 const int gain = 16000;
+const int CHUNK_SIZE = 1024;
+
 
 class Sender{
     private:
@@ -89,6 +96,7 @@ class Sender{
             conn_sock = accept(listen_sock, (struct sockaddr*)&cli_addr, &cli_len);
 
             close(listen_sock);
+            listen_sock = -1;
 
             if(conn_sock < 0)
             {
@@ -105,7 +113,9 @@ class Sender{
             if(conn_sock < 0)
                 return false;
             //ssize_t muze ulozit i -1 [more you know :)]
-            ssize_t n_bytes = write(conn_sock, data, size_data);
+            //ssize_t n_bytes = write(conn_sock, data, size_data);
+            ssize_t n_bytes = send(conn_sock, data, size_data, MSG_NOSIGNAL);
+
             if(n_bytes != size_data)
             {
                 std::cout << "client disconnected" << std::endl;
@@ -221,39 +231,227 @@ class Channel{
         }
 };
 
-void radioWorker(int port, float offset)
-{}
+struct IQBlock
+{
+    std::vector<float> i_data;
+    std::vector<float> q_data;
+    IQBlock()
+    {
+        i_data.reserve(CHUNK_SIZE);
+        q_data.reserve(CHUNK_SIZE);
+    }
+};
 
+class RadioWorker
+{
+    private:
+        int port;
+        int offset;
+        bool running;
+        std::unique_ptr<Sender> sender;
+        std::unique_ptr<Channel> channel;
+        std::thread workerThread;
+
+        std::queue<std::shared_ptr<IQBlock>> dataQueue;
+        std::mutex queueMutex;
+        std::condition_variable queueCv;
+
+        const size_t MAX_QUEUE_SIZE = 50; 
+
+        void threatLoop()
+        {
+            sender = std::make_unique<Sender>(port);
+            if(!sender->startServerAccept())
+            {
+                running = false;
+                return;
+            }
+
+            channel = std::make_unique<Channel>(offset, sender.get());
+
+            while(running)
+            {
+                std::shared_ptr<IQBlock> block;
+                { // zamek zanikne po ukonceni scope
+                    std::unique_lock<std::mutex> lock(queueMutex);
+                    // cekani na data
+                    queueCv.wait(lock, [this] {return !dataQueue.empty() || !running;});
+                    if(!running && dataQueue.empty())
+                        break;
+                
+                    if(!dataQueue.empty())
+                    {
+                        block = dataQueue.front();
+                        dataQueue.pop();
+                    }
+                    if(block)
+                    {
+                        for(size_t k = 0; k < block->i_data.size(); ++k)
+                        {
+                            // klient se odpojil
+                            if(!channel->process(block->i_data[k], block->q_data[k]))
+                            {
+                                std::cout << "[Worker " << port << "] Processing stopped (client disconnected)." << std::endl;
+                                running = false;
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+    public:
+        RadioWorker(int port, float offset) : port(port), offset(offset), running(true)
+        {}
+        bool isRunning()
+        {return running;}
+        void start()
+        {workerThread = std::thread(&RadioWorker::threatLoop, this);}
+        void stop()
+        {
+            {
+                std::lock_guard<std::mutex> lock(queueMutex);
+                running = false;
+            }
+            queueCv.notify_all();
+
+            if(workerThread.joinable())
+                workerThread.join();
+        }
+        void pushData(const std::shared_ptr<IQBlock>& block)
+        {
+            std::lock_guard<std::mutex> lock(queueMutex);
+            if(!running)
+                return;
+
+            if(dataQueue.size() >= MAX_QUEUE_SIZE)
+                dataQueue.pop();
+
+            dataQueue.push(block);
+            queueCv.notify_one();
+        }
+};
+
+std::vector<std::shared_ptr<RadioWorker>> workers;
+std::mutex workersMutex;
+
+void httpServerLoop(int port)
+{
+    int srv_sock = socket(AF_INET, SOCK_STREAM, 0);
+    int opt = 1;
+    setsockopt(srv_sock, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+    
+    struct sockaddr_in addr;
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    addr.sin_port = htons(port);
+    
+    if(bind(srv_sock, (struct sockaddr*)&addr, sizeof(addr)) < 0)
+    {
+        std::cerr << "HTTP Server: Bind failed on port " << port << std::endl;
+        return;
+    }
+    listen(srv_sock, 5);
+    std::cout << "HTTP Server listening on port " << port << std::endl;
+
+    while(true)
+    {
+        struct sockaddr_in cli_addr;
+        socklen_t len = sizeof(cli_addr);
+        int cli_sock = accept(srv_sock, (struct sockaddr*)&cli_addr, &len);
+        if (cli_sock < 0) continue;
+
+        char buffer[1024] = {0};
+        read(cli_sock, buffer, 1024);
+        std::string request(buffer);
+        
+        std::string response = "HTTP/1.1 400 Bad Request\r\n\r\n";
+        
+        // parsing: GET /start?port=1234&offset=50000
+        if(request.find("GET") == 0)
+        {
+            int new_port = 0;
+            float new_offset = 0.0f;
+            
+            size_t pos_port = request.find("port=");
+            size_t pos_off = request.find("offset=");
+            
+            if(pos_port != std::string::npos && pos_off != std::string::npos)
+            {
+                new_port = std::stoi(request.substr(pos_port + 5));
+                new_offset = std::stof(request.substr(pos_off + 7));
+                
+                std::cout << "HTTP: Spawning worker on port " << new_port << ", offset " << new_offset << std::endl;
+                
+                auto worker = std::make_shared<RadioWorker>(new_port, new_offset);
+                worker->start();
+                
+                {
+                    std::lock_guard<std::mutex> lock(workersMutex);
+                    workers.push_back(worker);
+                }
+                
+                response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\nWorker started.";
+            }
+        }
+        
+        write(cli_sock, response.c_str(), response.length());
+        close(cli_sock);
+    }
+    close(srv_sock);
+}
 
 int main()
 {
-    uint8_t iq[2];
-    float i, q;
-    float demod, audio;
-    Sender* sndr0 = new Sender(8008);
-    if(!sndr0->startServerAccept())
-    {
-        std::cerr << "server did not started" << std::endl;
-        return 1;
-    }
-    Channel* chnl0 = new Channel(-200000.0f,sndr0);
-    Sender* sndr1 = new Sender(8009);
-    if(!sndr1->startServerAccept())
-    {
-        std::cerr << "server did not started" << std::endl;
-        return 1;
-    }
-    Channel* chnl1 = new Channel(400000.0f,sndr1);
+    std::thread httpThread(httpServerLoop, 8080);
+    httpThread.detach();
 
-    while(std::cin.read(reinterpret_cast<char*>(iq),2))
+    const int SAMPLES_PER_CHUNK = 1024;
+    uint8_t raw_buffer[SAMPLES_PER_CHUNK * 2]; 
+
+    std::cout << "Core system started. Feed IQ data to stdin..." << std::endl;
+    std::cout << "Use HTTP to spawn workers: curl 'http://localhost:8080/start?port=8008&offset=-200000'" << std::endl;
+
+    while(std::cin.read(reinterpret_cast<char*>(raw_buffer), sizeof(raw_buffer)))
     {
-        i = ((float)iq[0] - 127.5f) / 127.5f;
-        q = ((float)iq[1] - 127.5f) / 127.5f;
-        if(!chnl0->process(i,q))
-            break;
-        if(!chnl1->process(i,q))
-            break;
+        auto block = std::make_shared<IQBlock>();
+
+        // mohlo by byt v intech a ne ve floatech :)
+        for(int i = 0; i < SAMPLES_PER_CHUNK; ++i)
+        {
+            float i_val = ((float)raw_buffer[2*i] - 127.5f) / 127.5f;
+            float q_val = ((float)raw_buffer[2*i+1] - 127.5f) / 127.5f;
+            block->i_data.push_back(i_val);
+            block->q_data.push_back(q_val);
+        }
+
+        {
+            std::lock_guard<std::mutex> lock(workersMutex);
+            // pres iteratory, protoze je potom mazu
+            // producer -> consumer
+            for(auto it = workers.begin(); it != workers.end();)
+            {
+                if((*it)->isRunning())
+                {
+                    (*it)->pushData(block);
+                    ++it;
+                }
+                else
+                {
+                    (*it)->stop();
+                    it = workers.erase(it);
+                }
+            }
+        }
     }
-    std::cout << "exiting program" << std::endl;
+
+    std::cout << "Input stream ended. Shutting down." << std::endl;
+    {
+        std::lock_guard<std::mutex> lock(workersMutex);
+        for(auto& w : workers)
+            w->stop();
+    }
+    
     return 0;
 }
